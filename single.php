90971305d86dafe9b31413969ab75cfd7a04763d
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-12-06 22:53:06 +0100

    no message

--- single.php
+++ single.php
@@ -10,7 +10,7 @@
 
     		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>	
 	    		<h1><?php the_title(); ?></h1>	
-	    		<h4 style="margin:15px 0 30px 0"><?php the_time("F jS, Y") ?> by <?php the_author(); ?></h4>
+	    		<h4 style="margin:15px 0 30px 0"><?php the_time("F jS, Y") ?> written by <?php the_author(); ?></h4>
 	    		<?php the_content(); ?>
 	    	<?php endwhile; endif; ?>
     	</div>	
