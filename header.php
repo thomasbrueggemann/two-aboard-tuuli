4f2d11a6ffd7e99700f7a98605e698e83e8ce217
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-12-06 15:23:34 +0100

    thin out header

--- header.php
+++ header.php
@@ -6,30 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="description" content="<?php bloginfo("description") ?>">
-    <meta name="keywords" content="">
+    <meta name="keywords" content="two aboard tuuli,tuuli,sailing,segeln,sailboat,segelboot,liveaboard,circumnavigate,cruising,bluewater,blauwasser">
     <meta name="author" content="Thomas Brüggemann" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
     <?php wp_head(); ?>
-
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?php bloginfo("stylesheet_directory"); ?>/img/favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
 
     <link rel="stylesheet" href="<?php bloginfo("stylesheet_directory"); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php bloginfo("stylesheet_directory"); ?>/css/font-awesome.min.css">
