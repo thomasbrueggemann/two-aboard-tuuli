365b0c808b37b25555ff4789c42418f75749c88e
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-12-06 21:28:25 +0100

    center

--- footer.php
+++ footer.php
@@ -22,6 +22,6 @@
 		ga("create", "UA-15981085-15", "auto");
 		ga("send", "pageview");
     </script>
-    <script language="javascript1.1" src="http://www.sail-3d.com/toplist/add.js.php?id=2160405"></script>
+    <center><script language="javascript1.1" src="http://www.sail-3d.com/toplist/add.js.php?id=2160405"></script></center>
 </body>
 </html>
