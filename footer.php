caac3d2566582c7ee76c158c11625665760f7064
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-12-06 01:34:56 +0100

    GA

--- footer.php
+++ footer.php
@@ -15,5 +15,15 @@
 			});
 		});
 	</script>
+	<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-15981085-15', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <script language="javascript1.1" src="http://www.sail-3d.com/toplist/add.js.php?id=2160405">
+	</script>
 </body>
 </html>
