1645eb28ee5a57d329beca2530cd882331681d5d
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-12-09 10:34:37 +0100

    ssl

--- sidebar.php
+++ sidebar.php
@@ -34,7 +34,7 @@
 		<i class="fa fa-youtube"></i>
 	</a>
 	<a href="http://cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Ftwo-aboard-tuuli.com/rss" target="blank" class="icon-feedly">
-		<img id="feedlyFollow" src="http://s3.feedly.com/img/follows/feedly-follow-circle-flat-green_2x.png" alt="follow us in feedly" width="31" height="31">
+		<img id="feedlyFollow" src="//s3.feedly.com/img/follows/feedly-follow-circle-flat-green_2x.png" alt="follow us in feedly" width="31" height="31">
 	</a>
 </section>
 
